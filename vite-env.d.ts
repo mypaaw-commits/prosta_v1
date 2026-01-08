e258b35c5d6e80de96766a3cac4d85f090eb863c
mypaaw-commits <mypaaw@gmail.com>
2026-01-08 22:49:57 +0100

    build: Remove redundant tsc call from build script
    The `tsc` command is no longer needed as Vite handles TypeScript compilation during the build process. This simplifies the build script and removes a potential point of failure.
    
    Also, added type declarations for various image file types and Vite environment variables.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,23 @@
-// /// <reference types="vite/client" />
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.webp';
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  [key: string]: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
